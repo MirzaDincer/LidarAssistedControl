ae48d28632dc604b2cbedfaf84531c91584f768a
fede9936 <demitafederico@gmail.com>
2026-02-07 18:59:15 +0100

    updates IPC with optimized Kp and Ti

--- Sprint/RunExample_Simulink.m
+++ Sprint/RunExample_Simulink.m
@@ -54,10 +54,9 @@ simu.dt             = P.FP.Val{contains(P.FP.Label,'DT')};
 [R,F]               = load_ROSCO_params(P,simu);
 
 % Individual pitch controller
-R.IPC_ControlMode = 1;
-R.IPC_KP_1P = 1e-9;
-R.IPC_KI_1P = 20;
-R.IPC_TI =R.IPC_KP_1P/R.IPC_KI_1P;
+IPC = [];
+IPC.Kp = 5.3e-7;
+IPC.Ti = 4;
 
 % add FF Parameter from FFP_v1.IN
 R.StaticWind        = [0   10.0000   11.0000   12.0000   13.0000   14.0000   15.0000   16.0000   17.0000   18.0000   19.0000   20.0000   21.0000   22.0000   23.0000   24.0000   25.0000   26.0000   27.0000   28.0000   29.0000   30.0000]; % Wind speed  values in static pitch curve [m/s]
@@ -67,7 +66,7 @@ R.StaticPitch       = [0         0    0.0552    0.1085    0.1451    0.1749    0.
 clear FAST_SFunc 
 clear OpenFAST_ROSCO_LDP_FFP
 R.FlagLAC           = 0; % Disable LAC
-SimOutFB            = sim('OpenFAST_ROSCO_LDP_FFP_with_IPC.slx',[0,TMax]);
+SimOutFB            = sim('OpenFAST_ROSCO_LDP_FFP.slx',[0,TMax]);
 movefile([SimulationName,'.SFunc.outb'],[SimulationName,'_FB.outb'])      % store results
 
 %% Run FBFF
