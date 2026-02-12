a73ec097d8ebe2769cf4bf737cfe333bea6196cb
fede9936 <demitafederico@gmail.com>
2026-02-12 15:47:13 +0100

    new FF IPC

--- Sprint/RunExample_Simulink.m
+++ Sprint/RunExample_Simulink.m
@@ -15,10 +15,15 @@ addpath(genpath('..\WetiMatlabFunctions'))
 addpath(genpath('..\NrelMatlabFunctions'))
 
 % select simulated lidar
-LidarType       = 'CircularCW'; % [4BeamPulsed/CircularCW]
+LidarType       = '4BeamPulsed'; % [4BeamPulsed/CircularCW]
 
 % simulation time
 TMax                = 50; % [s]
+
+% IPC parameters
+IPC = [];
+IPC.FF.gV = 1; %static gain for Vertical component
+IPC.FF.gH = 1; %static gain for Horizontal component
 
 switch LidarType
     case '4BeamPulsed'
@@ -29,6 +34,8 @@ switch LidarType
         LDP.FlagLPF             = 1;            % [0/1]     Enable low-pass filter (flag)
         LDP.omega_cutoff        = 0.1232;       % [rad/s]   Corner frequency (-3dB) of the low-pass filter
         LDP.T_buffer            = 5.5;          % [s]       Buffer time for filtered REWS signal
+        IPC.FB.Kp = 5.3e-7;
+        IPC.FB.Ti = 4;
     case 'CircularCW'
         % configuration from LDP_v1_CircularCW.IN and FFP_v1_CircularCW.IN
         LDP.NumberOfBeams       = 50;           % [-]       Number of beams measuring at different directions               
@@ -38,7 +45,10 @@ switch LidarType
         LDP.omega_cutoff        = 0.3268;       % [rad/s]   Corner frequency (-3dB) of the low-pass filter
         % LDP.omega_cutoff        = 0.5;       % [rad/s]   Corner frequency (-3dB) of the low-pass filter
         LDP.T_buffer            = 7.5;          % [s]       Buffer time for filtered REWS signal        
-        % LDP.T_buffer            = 7;          % [s]       Buffer time for filtered REWS signal        
+        % LDP.T_buffer            = 7;          % [s]       Buffer time for filtered REWS signal     
+        % Individual pitch controller
+        IPC.FB.Kp = 5.3e-7;
+        IPC.FB.Ti = 4;
 end
 
 
@@ -53,10 +63,6 @@ P                   = ReadWrite_FAST(fast);
 simu.dt             = P.FP.Val{contains(P.FP.Label,'DT')};
 [R,F]               = load_ROSCO_params(P,simu);
 
-% Individual pitch controller
-IPC = [];
-IPC.Kp = 5.3e-7;
-IPC.Ti = 4;
 
 % add FF Parameter from FFP_v1.IN
 R.StaticWind        = [0   10.0000   11.0000   12.0000   13.0000   14.0000   15.0000   16.0000   17.0000   18.0000   19.0000   20.0000   21.0000   22.0000   23.0000   24.0000   25.0000   26.0000   27.0000   28.0000   29.0000   30.0000]; % Wind speed  values in static pitch curve [m/s]
@@ -69,17 +75,33 @@ R.FlagLAC           = 0; % Disable LAC
 SimOutFB            = sim('OpenFAST_ROSCO_LDP_FFP.slx',[0,TMax]);
 movefile([SimulationName,'.SFunc.outb'],[SimulationName,'_FB.outb'])      % store results
 
+%% Run FB with IPC
+clear FAST_SFunc 
+clear OpenFAST_ROSCO_LDP_FFP_with_IPC
+R.FlagLAC           = 0; % Disable LAC
+SimOutFB            = sim('OpenFAST_ROSCO_LDP_FFP_with_IPC.slx',[0,TMax]);
+movefile([SimulationName,'.SFunc.outb'],[SimulationName,'_FBIPC.outb'])      % store results
+
 %% Run FBFF
 clear FAST_SFunc 
 clear OpenFAST_ROSCO_LDP_FFP
 R.FlagLAC           = 1; % Enable LAC
-SimOutFBFF          = sim('OpenFAST_ROSCO_LDP_FFP_with_IPC.slx',[0,TMax]);
+SimOutFBFF          = sim('OpenFAST_ROSCO_LDP_FFP.slx',[0,TMax]);
 movefile([SimulationName,'.SFunc.outb'],[SimulationName,'_FBFF.outb'])    % store results
+
+%% Run FBFF with IPC
+clear FAST_SFunc 
+clear OpenFAST_ROSCO_LDP_FFP_with_IPC
+R.FlagLAC           = 1; % Enable LAC
+SimOutFBFF          = sim('OpenFAST_ROSCO_LDP_FFP_with_IPC.slx',[0,TMax]);
+movefile([SimulationName,'.SFunc.outb'],[SimulationName,'_FBFFIPC.outb'])    % store results
 
 %% Comparison
 % read in data
 FB              = ReadFASTbinaryIntoStruct([SimulationName,'_FB.outb']);
 FBFF            = ReadFASTbinaryIntoStruct([SimulationName,'_FBFF.outb']);
+FBIPC           = ReadFASTbinaryIntoStruct([SimulationName,'_FBIPC.outb']);
+FBFFIPC         = ReadFASTbinaryIntoStruct([SimulationName,'_FBFFIPC.outb']);
 
 % Plot 
 figure('Name','Simulation results')
@@ -95,40 +117,46 @@ subplot(4,1,2);
 hold on; grid on; box on
 plot(FB.Time,       FB.BldPitch1);
 plot(FBFF.Time,     FBFF.BldPitch1);
+plot(FBIPC.Time,     FBIPC.BldPitch1);
+plot(FBFFIPC.Time,     FBFFIPC.BldPitch1);
 ylabel({'BldPitch1'; '[deg]'});
-legend('feedback only','feedback-feedforward','Location','best')
+legend('feedback only','feedback-feedforward','feedback only with IPC','feedback-feedforward with IPC' ,'Location','best')
 
 subplot(4,1,3);
 hold on; grid on; box on
 plot(FB.Time,       FB.RotSpeed);
 plot(FBFF.Time,     FBFF.RotSpeed);
+plot(FBIPC.Time,     FBIPC.RotSpeed);
+plot(FBFFIPC.Time,     FBFFIPC.RotSpeed);
 ylabel({'RotSpeed';'[rpm]'});
 
 subplot(4,1,4);
 hold on; grid on; box on
 plot(FB.Time,       FB.TwrBsMyt/1e3);
 plot(FBFF.Time,     FBFF.TwrBsMyt/1e3);
+plot(FBIPC.Time,     FBIPC.TwrBsMyt/1e3);
+plot(FBFFIPC.Time,     FBFFIPC.TwrBsMyt/1e3);
 ylabel({'TwrBsMyt';'[MNm]'});
 
 xlabel('time [s]')
 linkaxes(findobj(gcf, 'Type', 'Axes'),'x');
 xlim([20 50])
 
-figure(4);
-hold on; grid on; box on
-plot(FBFF.Time,     FBFF.RootMyb1);
-plot(FBFF.Time,     FBFF.RootMyb2);
-plot(FBFF.Time,     FBFF.RootMyb3);
-ylabel({'RootMyb'; '[deg]'});
-legend('feedback only','feedback-feedforward','Location','best')
-
-figure(5);
-hold on; grid on; box on
-plot(FBFF.Time,     FBFF.BldPitch1);
-plot(FBFF.Time,     FBFF.BldPitch2);
-plot(FBFF.Time,     FBFF.BldPitch3);
-ylabel({'RootMyb'; '[deg]'});
-legend('feedback only','feedback-feedforward','Location','best')
+% figure(4);
+% hold on; grid on; box on
+% plot(FBFF.Time,     FBFF.RootMyb1);
+% plot(FBFF.Time,     FBFF.RootMyb2);
+% plot(FBFF.Time,     FBFF.RootMyb3);
+% ylabel({'RootMyb'; '[deg]'});
+% legend('feedback only','feedback-feedforward','Location','best')
+% 
+% figure(5);
+% hold on; grid on; box on
+% plot(FBFF.Time,     FBFF.BldPitch1);
+% plot(FBFF.Time,     FBFF.BldPitch2);
+% plot(FBFF.Time,     FBFF.BldPitch3);
+% ylabel({'RootMyb'; '[deg]'});
+% legend('feedback only','feedback-feedforward','Location','best')
 
 
 % display results
@@ -136,7 +164,11 @@ RotSpeed_0  = 7.56;     % [rpm]
 TwrBsMyt_0  = 158.3e3;  % [kNm]
 t_Start     = 0;        % [s]
 
-Cost = (max(abs(FBFF.RotSpeed(FBFF.Time>=t_Start)-RotSpeed_0))) / RotSpeed_0 ...
-     + (max(abs(FBFF.TwrBsMyt(FBFF.Time>=t_Start)-TwrBsMyt_0))) / TwrBsMyt_0;
+Cost_FB = (max(abs(FB.RotSpeed(FB.Time>=t_Start)-RotSpeed_0))) / RotSpeed_0 ...
+     + (max(abs(FB.TwrBsMyt(FB.Time>=t_Start)-TwrBsMyt_0))) / TwrBsMyt_0;
 
+Cost = (max(abs(FBFFIPC.RotSpeed(FBFFIPC.Time>=t_Start)-RotSpeed_0))) / RotSpeed_0 ...
+     + (max(abs(FBFFIPC.TwrBsMyt(FBFFIPC.Time>=t_Start)-TwrBsMyt_0))) / TwrBsMyt_0;
+
+fprintf('Cost for Summer Games 2024 (feedback only) ("30 s sprint"):  %f \n',Cost_FB);
 fprintf('Cost for Summer Games 2024 ("30 s sprint"):  %f \n',Cost);
